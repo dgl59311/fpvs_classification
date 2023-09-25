93063599967775d494175d3972cb32cba563d521
dgl59311 <darioalejj10@gmail.com>
2023-09-25 16:10:49 +0200

    Initial commit

---
+++ analysis_expt1.py
@@ -0,0 +1,149 @@
+# plot and generate results
+import os
+import pingouin as pg
+import pandas as pd
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from funcs.dependencies_clf import experiment_info
+# pip install --upgrade PyQt5
+
+matplotlib.use('Qt5Agg')
+pd.options.mode.chained_assignment = None  # default='warn'
+main_dir = os.getcwd()
+results_dir = os.path.join(main_dir, 'results')
+
+# read demog file
+demog_file = pd.read_csv('SR_Testing_FPVS.csv', index_col=0)
+conditions, clf_files_local, id_files = experiment_info(1)
+
+# frequencies to analyze
+freq_vector = np.arange(0, 257, 1)
+freq_vector = freq_vector[freq_vector < 48]
+
+# colors
+pl_colors = ['r', 'b', 'g', 'm']
+# set to 1 to plot random label permuted data
+plot_perm = 0
+cm = 1/2.54  # centimeters in inches
+
+# results from experiment
+results = list(filter(lambda s: 'experiment_1.npy' in s, os.listdir(results_dir)))
+rlp = list(filter(lambda s: 'experiment_1rlp' in s, os.listdir(results_dir)))
+sdt = list(filter(lambda s: 'experiment_1tp_fp_fn_tn' in s, os.listdir(results_dir)))
+
+for id_ in range(26):
+
+    subj = results[id_]
+    subj_id = subj.replace('_experiment_1.npy', '')
+    data_ = np.load(os.path.join(results_dir, subj))
+    rlp_ = np.load(os.path.join(results_dir, rlp[id_]))
+    sdt_ = np.load(os.path.join(results_dir, sdt[id_]))
+    group_ = demog_file.loc[subj_id].Label
+
+    for j in range(len(conditions)):
+        if 'Control' in group_:
+            g_id = 'control'
+        else:
+            g_id = 'sr'
+
+        # create dataframes
+        prc_ = np.percentile(data_[:, j, :], (25, 50, 75), axis=0)
+        prc_[0] = prc_[1] - prc_[0]
+        prc_[2] = prc_[2] - prc_[1]
+
+        df_tmp = pd.DataFrame([prc_[1]])
+        df_tmp.columns = freq_vector + 1
+        df_tmp.index = [subj_id]
+        df_tmp['group'] = g_id
+        df_tmp['condition'] = conditions[j]
+
+        if id_ == 0 and j == 0:
+            data_cat = df_tmp
+        else:
+            data_cat = pd.concat([data_cat, df_tmp])
+
+# get d prime confidence intervals
+for i in range(len(conditions)):
+    plt.figure(figsize=(15 * cm, 9 * cm))
+    plt.suptitle(conditions[i])
+    repetitions = 1000
+    filt_condition = data_cat[data_cat['condition'] == conditions[i]]
+    control = filt_condition[filt_condition['group'] == 'control']
+    sr = filt_condition[filt_condition['group'] == 'sr']
+    con_true = [control[i].mean() for i in range(1, 49)]
+    sr_true = [sr[i].mean() for i in range(1, 49)]
+    eff_size_true = [pg.compute_effsize(control[i], sr[i]) for i in range(1, 49)]
+    pval_true = [pg.ttest(control[i], sr[i])['p-val'][0] for i in range(1, 49)]
+    shuff_d = np.zeros((repetitions, len(freq_vector)))
+
+    for nr in range(repetitions):
+        shuffled_column = np.random.permutation(filt_condition['group'])
+        filt_condition.loc[:, 'group'] = shuffled_column
+        control = filt_condition[filt_condition['group'] == 'control']
+        sr = filt_condition[filt_condition['group'] == 'sr']
+        diff = [control[i].mean() - sr[i].mean() for i in range(1, 49)]
+        shuff_d[nr, :] = np.array(diff)
+
+    ci_ = np.abs(np.percentile(shuff_d, (2.5, 97.5), axis=0))
+    plt.scatter(freq_vector + 1 - 0.1, con_true, s=30, facecolors='none', edgecolors='blue')
+    plt.errorbar(freq_vector + 1 - 0.1, con_true, yerr=ci_, capsize=2, color='blue', elinewidth=0.5, linewidth=0.7)
+    plt.scatter(freq_vector + 1 + 0.1, sr_true, s=30, facecolors='none', edgecolors='red')
+    plt.errorbar(freq_vector + 1 + 0.1, sr_true, yerr=ci_, capsize=2, color='red', elinewidth=0.5, linewidth=0.7)
+
+    plt.legend(['controls', 'SRs'])
+    plt.xlabel('frequency')
+    plt.ylabel("classifier sensitivity d'")
+
+    plt.ylim([-0.4, 2])
+
+    plt.tight_layout()
+    plt.savefig(os.path.join(main_dir, 'Experiment_1_' + conditions[i] + '.jpg'), dpi=500)
+
+# Plot individual subject data
+plt.figure(figsize=(18*cm, 18*cm))
+plt.suptitle('Experiment 1 - 3')
+for id_ in range(18, 26):
+
+    subj = results[id_]
+    subj_id = subj.replace('_experiment_1.npy', '')
+    data_ = np.load(os.path.join(results_dir, subj))
+    rlp_ = np.load(os.path.join(results_dir, rlp[id_]))
+    group_ = demog_file.loc[subj_id].Label
+
+    for j in range(len(conditions)):
+        if id_ < 9:
+            id_pl = id_
+        else:
+            id_pl = id_ - 18
+        plt.subplot(3, 3, id_pl + 1)
+        # true data
+        prc_ = np.percentile(data_[:, j, :], (25, 50, 75), axis=0)
+        prc_[0] = prc_[1] - prc_[0]
+        prc_[2] = prc_[2] - prc_[1]
+        plt.scatter(freq_vector + 1, prc_[1], s=5, c=pl_colors[j])
+        plt.errorbar(freq_vector + 1, prc_[1], yerr=prc_[[0, 2]], capsize=2, color=pl_colors[j], elinewidth=0.5, linewidth=0.7)
+        # perm data
+        if plot_perm == 1:
+            prc_ = np.percentile(rlp_[:, j, :], (25, 50, 75), axis=0)
+            prc_[0] = prc_[1] - prc_[0]
+            prc_[2] = prc_[2] - prc_[1]
+            plt.scatter(freq_vector + 1, prc_[1], s=5, c=pl_colors[j])
+            plt.errorbar(freq_vector + 1, prc_[1], yerr=prc_[[0, 2]], capsize=2, color=pl_colors[j], elinewidth=0.5, linewidth=0.7)
+
+        plt.ylabel("d'")
+        plt.xlabel('frequency')
+        plt.ylim([-0.5, 4])
+        plt.xlim(-1, 50)
+        if 'Control' in group_:
+            g_id = 'control'
+        else:
+            g_id = 'sr'
+
+        plt.text(-0.5, 3.5, g_id)
+
+    plt.legend(conditions, loc="upper right", ncol=1, fontsize=7)
+
+plt.tight_layout()
+plt.subplots_adjust(wspace=0.3, hspace=0.3)
+plt.savefig(os.path.join(main_dir, 'Experiment_1_' + 'subj_18-26' + '.jpg'), dpi=500)
---
+++ funcs/dependencies_clf.py
@@ -0,0 +1,112 @@
+import os
+import pandas as pd
+import scipy.io as sio
+import scipy.stats as stats
+import numpy as np
+from sklearn.metrics import confusion_matrix
+from sklearn.base import clone
+
+
+def experiment_info(id_experiment):
+    if id_experiment == 1:
+        conditions = ['03Hz', '06Hz', '09Hz', '12Hz']
+        dir_files = "C:/Users/gordillo/Desktop/local_fpvs/Experiment 1"
+        id_files = '_experiment_1'
+
+    elif id_experiment == 2:
+        conditions = ['house', 'face']
+        dir_files = "C:/Users/gordillo/Desktop/local_fpvs/Experiment 2"
+        id_files = '_experiment_2'
+    else:
+        conditions = ['10Hz050', '10Hz100', '20Hz100']
+        dir_files = "C:/Users/gordillo/Desktop/local_fpvs/Experiment 3"
+        id_files = '_experiment_3'
+    return conditions, dir_files, id_files
+
+
+def data_check(conditions, folder):
+    # the output is a dataframe with the name of the participant and the location of the data for each condition
+    for i in range(len(conditions)):
+        list_files = list(filter(lambda s: '.mat' in s and conditions[i] in s, os.listdir(folder)))
+        common_prefix = os.path.commonprefix(list_files)
+        common_suffix = os.path.commonprefix([s[::-1] for s in list_files])[::-1]
+        result_list = [s.replace(common_prefix, '').replace(common_suffix, '') for s in list_files]
+        if i == 0:
+            df_results = pd.DataFrame(index=result_list, data=list_files, columns=[conditions[i]])
+        else:
+            tmp_df = pd.DataFrame(index=result_list, data=list_files, columns=[conditions[i]])
+            df_results = pd.concat([df_results, tmp_df], axis=1, sort=False)
+    return df_results
+
+
+def find_headers(matfilepath):
+    header = sio.loadmat(matfilepath.replace(".mat", ".lw6"))['header']
+    # number of epochs available
+    epochs = header['datasize'].item()[0][0]
+    xstart = header['xstart'].item()[0][0]
+    xstep = header['xstep'].item()[0][0]
+    xlen = header['datasize'].item()[0][5]
+    return epochs, xstart, xstep, xlen
+
+
+def sdt_multinomial(confusion_mat):
+
+    num_classes = confusion_mat.shape[0]
+    sdt_values = np.zeros((num_classes, 4))
+
+    for i in range(num_classes):
+
+        tp = confusion_mat[i, i]
+        fp = np.sum(confusion_mat[:, i]) - tp
+        fn = np.sum(confusion_mat[i, :]) - tp
+        tn = np.sum(confusion_mat) - (tp + fp + fn)
+
+        sdt_values[i, 0] = tp
+        sdt_values[i, 1] = fp
+        sdt_values[i, 2] = fn
+        sdt_values[i, 3] = tn
+
+    return sdt_values
+
+
+def dprime_clf(tp, fp, fn, tn):
+
+    # loglinear dprime
+    H = (tp + 0.5) / (tp + fn + 1)
+    FA = (fp + 0.5) / (fp + tn + 1)
+
+    Z_H = stats.norm.ppf(H)
+    Z_FA = stats.norm.ppf(FA)
+    d_prime = Z_H - Z_FA
+
+    return d_prime
+
+
+def clf_fpvs(train_index, test_index, model, data_x, data_y, freq):
+    eeg_train = data_x[train_index]
+    cat_train = data_y[train_index]
+    eeg_test = data_x[test_index]
+    cat_test = data_y[test_index]
+    n_cat = len(np.unique(data_y))
+    results_ = np.zeros((n_cat, 4, len(freq)))
+    d_primes = np.zeros((n_cat, len(freq)))
+    for i in range(len(freq)):
+        freq_ = freq[i]
+        freq_train = eeg_train[:, :, freq_]
+        freq_test = eeg_test[:, :, freq_]
+        # fit classifier on train data with cross-validation
+        internal_clf = clone(model)
+        fit_model = internal_clf.fit(freq_train, cat_train)
+        prediction_ = fit_model.predict(freq_test)
+        if n_cat > 2:
+            conf_mat = confusion_matrix(cat_test, prediction_)
+            # gives a matrix of category x sdt measure (TP, FP, FN, TN)
+            sdt_ = sdt_multinomial(conf_mat)
+            results_[:, :, i] = sdt_
+            for j in range(n_cat):
+                d_primes[j, i] = dprime_clf(sdt_[j, 0], sdt_[j, 1], sdt_[j, 2], sdt_[j, 3])
+        else:
+            tn, fp, fn, tp = confusion_matrix(cat_test, prediction_).ravel()
+            d_primes[0, i] = dprime_clf(tp, fp, fn, tn)
+
+    return results_, d_primes
